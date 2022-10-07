dbaaf1cc0d1a8342adcfaa7784b84897976fef0f
Oluyemi Paul <oluola96@gmail,com>
2022-10-07 05:31:19 +0100

    Feat:

---
+++ src/components/__tests__/Details.test.jsx
@@ -0,0 +1,20 @@
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../redux/configureStore';
+import Details from '../Details';
+import { coinDetail } from '../../redux/CoinState/detailReducer';
+
+describe('Details component', () => {
+  test('component match with the snapshot', async () => {
+    await store.dispatch(coinDetail('bitcoin'));
+    const tree = renderer.create(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Details />
+        </Provider>
+      </BrowserRouter>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
